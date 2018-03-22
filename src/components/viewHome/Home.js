b519753ff51b1fbd070e5ad875af28a9ad38d098
Jessica Meza <jessicamezarojas2011@gmail.com>
2018-03-22 09:14:42 -0500

    borrando img

--- src/components/viewHome/Home.js
+++ src/components/viewHome/Home.js
@@ -1,9 +1,7 @@
 import React, {Component} from 'react';
-import Portada from '../../assets/images/portada.jpg';
-import DesiertoHuacachina from '../../assets/images/desierto-huacachina.jpeg';
-import Paracas from '../../assets/images/paracas.jpeg';
-import LagunaHuacachina from '../../assets/images/laguna-huacachina.jpeg';
-import Huaraz from '../../assets/images/huaraz.jpeg';
+// import Portada from '../../assets/images/portada.jpg';
+// import DesiertoHuacachina from '../../assets/images/desierto-huacachina.jpeg';
+// import Paracas from '../../assets/images/paracas.jpeg';
 
 import {Slider, Slide} from 'react-materialize'
 
@@ -11,7 +9,7 @@ class Home extends Component {
   render () {
     return (
       <div>
-      <Slider>
+      {/* <Slider>
       
       <Slide
         src={Portada}
@@ -27,7 +25,7 @@ class Home extends Component {
         title="Right aligned Caption"
         placement="center">
       </Slide>
-    </Slider>
+    </Slider> */}
    
     </div>
     )
